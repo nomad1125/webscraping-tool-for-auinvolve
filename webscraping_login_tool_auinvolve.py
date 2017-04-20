b9896b8cc496e37d878ecde0cd291f05b1c5e3d9
dsteinr8 <dsteinr8@gmail.com>
2017-04-19 23:35:10 -0500

    Add files via upload

---
+++ webscraping_login_tool_auinvolve.py
@@ -0,0 +1,5 @@
+# Title: webscraping_login_tool_auinvolve.py
+# Description: Prompts the user for the username and password that they desire
+#              to log in with.  The program will then log in to AUinvolve.
+# Author: David S Stein
+# Date: 19 April 2017
