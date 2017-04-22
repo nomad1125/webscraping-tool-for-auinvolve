66e6253af7a5b315ca993f5a4be245a0c4fc8c3e
Jarrett Tang <nomad1125@gmail.com>
2017-04-22 02:10:20 -0500

    basics working the way they should

--- webscraping_tool_auinvolve.py
+++ webscraping_tool_auinvolve.py
@@ -19,6 +19,10 @@ def paginationLimit(page_check):
 # if there is time add in both different options
 # def rosterExtraction1(main_soup):
 
+def fixClassNames(my_soup):
+    temp_containers = my_soup.findAll("tr", {"class" : "gridrow_alternate"})
+    for temp_container in temp_containers:
+        temp_container['class'] = 'gridrow'
 
 my_url = "https://auburn.collegiatelink.net/organization/ewb/roster/members"
 
@@ -32,17 +36,19 @@ main_soup = soup(page_html, "html.parser")
 
 # required ingredients
 MAX_PAGE = paginationLimit(main_soup)
-MAIN_URL = "https://auburn.collegiatelink.net"
-PARTNER_URL = "/organization/ewb/roster/members?Direction=Ascending&amp;page="
+MAIN_URL = 'https://auburn.collegiatelink.net'
+PARTNER_URL = '/organization/ewb/roster/members?Direction=Ascending&page='
 
 # file write setup
-filename = "auinvolve_roster_extraction.csv"
-f = open(filename, 'w')
-headers = "first name, last name, date joined"
-f.write(headers)
+# filename = "auinvolve_roster_extraction.csv"
+# f = open(filename, 'w')
+# headers = "First Name,Last Name,Member Since"
+# f.write(headers)
 
 # main loop
-for page_index in range(1, int(MAX_PAGE)):
+print("Max number of pages: " + MAX_PAGE)
+print("Game time...")
+for page_index in range(1, int(MAX_PAGE) + 1):
 
     # redirects url to roster only section for extraction
     # flips the pages for roster extraction
@@ -56,6 +62,9 @@ for page_index in range(1, int(MAX_PAGE)):
     # setting up soup recipe
     main_soup = soup(page_html, "html.parser")
 
+    # makes life easier
+    fixClassNames(main_soup)
+
     # grabbing soup ingredients
     name_containers = main_soup.findAll("tr", {"class" : "gridrow"})
 
@@ -64,11 +73,10 @@ for page_index in range(1, int(MAX_PAGE)):
     # idea: use concatenation to modify these data grabbing lines
     # instead of writing more than one function.
     for name_container in name_containers:
-
-        first_name = name_container.td.next_sibling.div.string
-        last_name = name_container.td.next_sibling.next_sibling.div.string
+        first_name = name_container.td.next_sibling.string
+        last_name = name_container.td.next_sibling.next_sibling.string
         date_joined = name_container.td.next_sibling.next_sibling.next_sibling.string
 
         print("first name: " + first_name)
         print("last name: " + last_name)
-        print("date joined: " + date_joined)
+        print("Member Since: " + date_joined)
