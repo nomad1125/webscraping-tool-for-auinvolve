7aeaefafe47588d69b3bf507dc0a745ef51a3e7f
Jarrett Tang <nomad1125@gmail.com>
2017-04-22 21:39:24 -0500

    not logged in version works and logged in version implemented

--- webscraping_tool_auinvolve.py
+++ webscraping_tool_auinvolve.py
@@ -7,9 +7,6 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-# login function here
-isLoggedIn = False
-
 # finding max cooking time for soup
 def paginationLimit(page_check):
     paginationContainer = page_check.findAll("div", {"class" : "pagination"})
@@ -17,13 +14,52 @@ def paginationLimit(page_check):
     page_number_location = len(str_temp) - 1
     ret_val = str_temp[page_number_location]
     return ret_val
-# if there is time add in both different options
-# def rosterExtraction1(main_soup):
 
+#roster extraction function if NOT logged into AU Involve
+def rosterExtractionNotLoggedIn(name_container):
+    first_name = name_container.td.next_sibling.string
+    last_name = name_container.td.next_sibling.next_sibling.string
+    date_joined = name_container.td.next_sibling.next_sibling.next_sibling.string
+
+    print("First Name: " + first_name)
+    print("Last Name: " + last_name)
+    print("Member Since: " + date_joined + "\n")
+
+    ret_val = first_name + "," + last_name + "," + date_joined + "\n"
+    return ret_val
+
+# roster extraction if logged into Auinvolve.
+# still needs to be adjust for the for loop.
+def rosterExtractionLoggedIn(name_container):
+    more_info = name_container.td.next_sibling.next_sibling.div.a['href']
+    first_name = name_container.td.next_sibling.next_sibling.div.string
+    last_name = name_container.td.next_sibling.next_sibling.next_sibling.next_sibling.div.string
+    date_joined = name_container.td.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.string
+
+    uClient = uReq(MAIN_URL + more_info)
+    temp_soup = soup(uClient.read(), "html.parser")
+    uClient.close()
+
+    temp_containers = temp_soup.findAll("div", {"class" : "userCard-info vcard"})
+    email_address = temp_containers[0].div.div.a.span.next_sibling.strip()
+
+    print("First Name: " + first_name)
+    print("Last Name: " + last_name)
+    print("Member Since: " + date_joined)
+    print("Email Address " + email_address + "\n")
+
+    ret_val = first_name + "," + last_name + "," + date_joined + "," + email_address + "\n"
+    return ret_val
+
+# this function saves lives by making all the search tags
+# the same making life easier.
 def fixClassNames(my_soup):
     temp_containers = my_soup.findAll("tr", {"class" : "gridrow_alternate"})
     for temp_container in temp_containers:
         temp_container['class'] = 'gridrow'
+
+# login function here
+isLoggedIn = False
 
 my_url = "https://auburn.collegiatelink.net/organization/ewb/roster/members"
 
@@ -43,8 +79,14 @@ PARTNER_URL = '/organization/ewb/roster/members?Direction=Ascending&page='
 # file write setup
 filename = "auinvolve_roster_extraction.csv"
 f = open(filename, 'w')
+header_extension = ""
 if isLoggedIn:
     header_extension = ",Email"
+
+else:
+    print("WARNING: Not logged into AU Involve\n" +
+          "\tSome names may be missing.\n")
+
 headers = "First Name,Last Name,Member Since" + header_extension +"\n"
 f.write(headers)
 
@@ -70,25 +112,13 @@ for page_index in range(1, int(MAX_PAGE) + 1):
     name_containers = main_soup.findAll("tr", {"class" : "gridrow"})
 
     # begin souping.
-    # setup specifically for a not logged in system.
-    # idea: use concatenation to modify these data grabbing lines
-    # instead of writing more than one function.
     for name_container in name_containers:
-        # first_name = name_container.td.next_sibling.string
-        # last_name = name_container.td.next_sibling.next_sibling.string
-        # date_joined = name_container.td.next_sibling.next_sibling.next_sibling.string
         if isLoggedIn:
-            print("stuff")
-            # change link???
+            print("logged in version missing...")
 
-        first_name = name_container.td.next_sibling.string
-        last_name = name_container.td.next_sibling.next_sibling.string
-        date_joined = name_container.td.next_sibling.next_sibling.next_sibling.string
+        else:
+            write_out = rosterExtractionNotLoggedIn(name_container)
 
-        print("First Name: " + first_name)
-        print("Last Name: " + last_name)
-        print("Member Since: " + date_joined + "\n")
-
-        f.write(first_name + "," + last_name + "," + date_joined + "\n")
+        f.write(write_out)
 
 f.close()
